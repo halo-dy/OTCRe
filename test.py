fd7aeb9abf9bb440c82df56cb6a8ffb85bdf4df4
halo-dy <dongye649@gmail.com>
2026-05-18 21:25:35 +0800

    数据集
    修正了代码中与论文不符的一点地方

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello git")
+import pandas as pd
+
+df = pd.read_pickle("data/raw/Singapore/Singapore_KG_plus.pkl")
+
+print(df.shape)
+print(df.columns)
+print(df.head())
+
+print(df['Brand'].value_counts().head(10))
+print(df['Region_ID'].value_counts().head(10))
+print(df.isnull().sum())
