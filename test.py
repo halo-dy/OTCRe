73d46e90189dad30e7868a47d237a2d3eb7ed685
halo-dy <dongye649@gmail.com>
2026-05-28 09:21:04 +0800

    修正

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_pickle("D:/作业/dLproj/OTCRe/OTCRe/data/raw/Singapore/Singapore_KG_plus.pkl")
-
-print(f"原始: {len(df)} POIs, {df['Brand'].nunique()} brands")
-print(df['Brand'].value_counts().describe())
